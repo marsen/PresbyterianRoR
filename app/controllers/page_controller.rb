178fa19cc4c87c092fd70473fbe7188f0600ee85
thisismygithub <thisismysoul@gmail.com>
2015-06-10 00:56:33 +0800

    add views and router

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -2,7 +2,20 @@ class PageController < ApplicationController
 	def index
 
 	end
+	def about
+
+	end
 	def mission
 		
+	end
+	def witness
+
+	end
+
+	def schedule
+
+	end
+	def contact
+
 	end
 end
