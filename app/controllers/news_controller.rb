523b76d3414abcf55a4e17b0d133a833f6e91d9a
thisismygithub <thisismysoul@gmail.com>
2015-07-02 02:13:00 +0800

    news crud

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -12,12 +12,17 @@ class NewsController < ApplicationController
 		redirect_to :action => :index
 
 	end
+	def update
+		@news = News.find(params[:id])
+		@news.update(news_params)
 
+		redirect_to :action => :show, :id => @news
+	end
 	def show
-
+		@news = News.find(params[:id])
 	end
 	def edit
-
+		@news = News.find(params[:id])
 	end
 
 	private
