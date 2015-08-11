62b2f1a53ce9fd5df1f70f2f853dc4e3d51a3160
thisismygithub <thisismysoul@gmail.com>
2015-08-11 19:49:34 +0800

    重構部份程式碼，見証分享接登入功能。

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,15 +1,15 @@
 class AdminController < ApplicationController
 	def index
-		@is_admin = to_bool(session["is_admin"])
+		@is_admin = to_bool(session[:is_admin])
 
 	end
 	def login
 		admin = Admin.where(:account => 'admin').first()
-		session["is_admin"] = admin.password == params[:password]
+		session[:is_admin] = admin.password == params[:password]
 		redirect_to :action => :index
 	end
 	def logout
-		session["is_admin"] = false
+		session[:is_admin] = false
 		redirect_to :action => :index
 	end
 	def to_bool(value)
