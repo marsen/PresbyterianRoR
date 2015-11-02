282e1f6f39c22170222ea2951f107248c4a34b71
Kevin <fdxyang@gmail.com>
2015-11-02 14:08:22 +0800

    test

--- db/schema.rb
+++ db/schema.rb
@@ -19,8 +19,8 @@ ActiveRecord::Schema.define(version: 20150809102410) do
   create_table "admins", force: :cascade do |t|
     t.string   "account"
     t.string   "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "news", force: :cascade do |t|
