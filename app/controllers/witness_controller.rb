d511b505a2a0d1bde667c94eac35ae3f0f62c194
thisismygithub <thisismysoul@gmail.com>
2015-12-07 00:44:24 +0800

    加PAGER

--- app/controllers/witness_controller.rb
+++ app/controllers/witness_controller.rb
@@ -1,8 +1,22 @@
 class WitnessController < ApplicationController
     def index
       #
-      @witnesses = Witness.where(is_active: true).order(:created_at).reverse_order.take(10)
-
+      @currentPage = 1
+      @pageSize = 10
+      if params[:p] != nil
+        @currentPage = params[:p].to_i
+      end
+      @activeWitness = Witness.where(is_active: true)
+      @witnesses = @activeWitness
+        .order(:created_at)
+        .reverse_order
+        .drop(@pageSize*(@currentPage-1))
+        .take(@pageSize)
+      @total = @activeWitness.count()
+      @totalPage = @total / @pageSize
+      if @total / @pageSize !=0
+        @totalPage = @totalPage + 1
+      end
     end
     def new
       @witness = Witness.new
